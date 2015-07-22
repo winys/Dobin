1021e083af88b11f2eadeb37c92bf1ca00c2bf5e
winys <824350760@qq.com>
2015-07-22 14:32:21 +0800

    update

--- dobin_test/App/Home/Controller/Index.js
+++ dobin_test/App/Home/Controller/Index.js
@@ -1,10 +1,47 @@
 module.exports = {
-	index : function () {
+	init : function () {
+	},
+	testFn : function () {
+		//var a = yield [1,2];
+		//console.log($.mysql);
+		this.end();
+	},
+	testGenFn : function *() {
+		//var b = yield ["testGenFn is called","too"];
+		var a = yield $.mysql_add("insert into test value('',7)");
+		this.echo(a);
+		this.echo("\n");
+
+		a =  yield $.mysql_getAll("select * from test");
+		this.echo(a);
+		this.echo("\n");
+
+		a = yield $.mysql_getOne("select * from test");
+		this.echo(a);
+		this.echo("\n");
+
+		a = yield $.mysql_update("update test set num = 7 where id=1");
+		this.echo(a);
+		this.echo("\n");
+
 		this.echo(this.ip());
 		this.end();
 	},
-	next : function () {
-		this.http.echo("<h1>QAQ</h1>猪");
-		this.http.end();
+	testRedis : function *() {
+		var a = yield $.redis("test","LaLaLa");
+		console.log(a);
+		this.echo(a);
+		this.echo(yield $.redis("test"));
+		a = yield $.redis({
+			test1:"ssss",
+			test2:"tttt"
+		});
+		this.echo(a);
+		this.echo(yield $.redis("test2"));
+		this.end();
+	},
+	testSession : function *() {
+		this.echo(yield this.session("var"));
+		this.end();
 	}
 }
